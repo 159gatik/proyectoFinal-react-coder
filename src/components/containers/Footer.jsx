7dfe1dc092ae6d9dd74399ba937af769993638e3
159gatik <rojasgaston95@gmail.com>
2022-10-06 22:00:44 -0300

    primer commit

---
+++ src/components/containers/Footer.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const Footer = () => {
+
+
+    return (
+        <>
+            <div className='footer '>
+                <div className='container'>
+
+                    <div className='d-flex row'>
+
+                        <div className='d-flex justify-content-evenly mt-5'>
+
+                            <div className='listStyle'>
+                                <ul>
+                                    <li><p>A</p></li>
+                                    <li><p>b</p></li>
+                                    <li><p>c</p></li>
+                                    <li><p>d</p></li>
+                                </ul>
+                            </div>
+                            <div className='listStyle'>
+                                <ul>
+                                    <li><p>PRODUCTS</p></li>
+                                    <li><p>bicicletas</p></li>
+                                    <li><p>accesorios</p></li>
+                                    <li><p>indumentaria</p></li>
+                                </ul>
+                            </div>
+                            <div className='listStyle'>
+                                <ul>
+                                    <li><p></p></li>
+                                    <li><p></p></li>
+                                    <li><p></p></li>
+                                    <li><p></p></li>
+                                </ul>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
